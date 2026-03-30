6b4366745ae3e1a622a339f281a9a4d9267c0d73
tatiane arantes <tatianearantessouza@gmail.com>
2026-03-30 20:23:08 -0300

    atualizacao apos aula  de 30/03/26

--- scripts/01-introducao.R
+++ scripts/01-introducao.R
@@ -2,8 +2,8 @@
 # DOCUMENTACAO DO SCRIPT
 # ============================================================
 # Arquivo: 01_introducao.R
-# Autor(a): <seu nome>
-# Data: <dd/mm/aaaa>
+# Autor(a): Tatiane Arantes
+# Data: 24/03/26
 # Objetivo: entender os fundamentos da linguagem R
 
 
--- scripts/02-importacao-manipulacao.R
+++ scripts/02-importacao-manipulacao.R
@@ -1,6 +1,6 @@
 # Arquivo: 02-importacao-manipulacao.R
-# Autor(a): <seu nome>
-# Data: <dd/mm/aaaa>
+# Autor(a): Tatiane Arantes
+# Data: 30/03/26
 # Objetivos:
 # 1. Importar um arquivo csv de dados
 # 2. Preparar os dados para análise
@@ -38,8 +38,7 @@ head(dados_vendas)
 # exibe as últimas linhas
 tail(dados_vendas)
 
-# resumo estatístico das colunas
-summary(dados_vendas)
+glimpse
 
 
 # Preparação dos dados ----------------------------------------------------
@@ -59,3 +58,48 @@ dados_vendas_limpos <- dados_vendas |>
 
 # verifica a estrutura dos dados
 glimpse(dados_vendas_limpos)
+
+
+#salva os dados limpos em um arquivo rds para analises futuras
+#sem precusar repetir a preparção dos dados
+
+#1. define o caminho relativo para salvar o arquivo rd
+caminho_rds <- here("dados/limpos/dados_vendas_limpos.rds")
+
+#2. salva o objeto dados_vendas_limpos no formato rds
+readr::write_rds(dados_vendas_limpos, caminho_rds)
+
+
+#1. define o caminho relativo para ler o arquivo rds
+caminho_rds <- here("dados/limpos/dados_vendas_limpos.rds")
+
+
+#2. lê o arquivo rds e armazena em um objeto
+dados_vendas_limpos <- readr::read_rds(caminho_rds)
+
+
+
+# A função filter ---------------------------------------------------------
+
+#filtra as vendas realizadas na cidade de "Formiga"
+dados_vendas_limpos |>
+  filter(cidade =="Formiga")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
